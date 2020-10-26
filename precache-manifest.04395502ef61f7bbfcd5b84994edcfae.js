da558168893309d26298e7effa5a9bffb9b39a78
ezekiel <ezekielnizamani0@gmail.com>
2020-10-26 06:23:32 -0700

    Updates

---
+++ precache-manifest.04395502ef61f7bbfcd5b84994edcfae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a5d6ff62945fa06fb869a29b1854b9a",
+    "url": "/ezekielnizamani/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "e8cef9489e234e004a5d",
+    "url": "/ezekielnizamani/covid-19-tracker/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "7d278da08f7b46ba004e",
+    "url": "/ezekielnizamani/covid-19-tracker/static/css/main.6bd960be.chunk.css"
+  },
+  {
+    "revision": "e8cef9489e234e004a5d",
+    "url": "/ezekielnizamani/covid-19-tracker/static/js/2.301604d8.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/ezekielnizamani/covid-19-tracker/static/js/2.301604d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d278da08f7b46ba004e",
+    "url": "/ezekielnizamani/covid-19-tracker/static/js/main.b2655176.chunk.js"
+  },
+  {
+    "revision": "0f2ef5270ccad08f6a02",
+    "url": "/ezekielnizamani/covid-19-tracker/static/js/runtime-main.e8cc1ff0.js"
+  }
+]);
